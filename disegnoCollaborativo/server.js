ef7e3f9ea5123f89d6f534dca39f4e1d568147d2
sashujke <jsickness1996@gmail.com>
2026-03-12 12:55:04 +0100

    Version 1.1

--- disegnoCollaborativo/server.js
+++ disegnoCollaborativo/server.js
@@ -5,19 +5,29 @@ const server = new WebSocketServer({ port });
 
 console.log("Server ws in ascolto su ws://localhost:"+port);
 
+const shapes = [];
+
 server.on("connection", (ws, req) => {
     const clientIp = req.socket.remoteAddress;
     console.log("Nuova connessione da "+clientIp);
-    ws.send("Benvenuto");
+
+    // invia la storia dei rettangoli al nuovo client
+    if (shapes.length > 0) {
+        ws.send(JSON.stringify(shapes));
+    }
 
     ws.on("message", async data => {
-        console.log("Ricevuto "+data+"da"+clientIp);
+        console.log("Ricevuto "+data+" da "+clientIp);
+        try {
+            const rect = JSON.parse(data);
+            shapes.push(rect);
+        } catch (_) {}
         server.clients.forEach(cli => {
             if (cli !== ws) cli.send(data, { binary: false });
         });
     });
 
-    ws.on("close", data => {
-        ws.send("Client disconnesso: " + clientIp);
+    ws.on("close", () => {
+        console.log("Client disconnesso: " + clientIp);
     });
 });
